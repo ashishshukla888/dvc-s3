123bb0ed78f3b1725ab3b53ba70d074dbd462c9d
ashish shukla <ashishshukla888.ak@com>
2024-07-02 14:25:24 +0530

    initial commit with PC2

---
+++ src/data_ingestion.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import os 
+
+df = pd.read_csv('./data/raw/dvc-s3-data.csv')
+
+X = df.drop(columns=['PLACED'])
+y = df['PLACED']
+
+
+# scaling the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Applying PCA
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_scaled)
+
+# Creating result with pca
+
+df_pca = pd.DataFrame(data=X_pca,columns=['PC1','PC2'])
+df_pca['PLACED'] = y.values
+
+df_pca.to_csv(os.path.join('data','processed','stud_perf_pca.csv'),index=False)
