d7faa3d4c965025c2dafc6cdc1e704569a3e29f8
ashish shukla <ashishshukla888.ak@com>
2024-07-02 14:55:41 +0530

    added PC3

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -14,12 +14,12 @@ scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
 # Applying PCA
-pca = PCA(n_components=2)
+pca = PCA(n_components=3)
 X_pca = pca.fit_transform(X_scaled)
 
 # Creating result with pca
 
-df_pca = pd.DataFrame(data=X_pca,columns=['PC1','PC2'])
+df_pca = pd.DataFrame(data=X_pca,columns=['PC1','PC2','PC3'])
 df_pca['PLACED'] = y.values
 
 df_pca.to_csv(os.path.join('data','processed','stud_perf_pca.csv'),index=False)
